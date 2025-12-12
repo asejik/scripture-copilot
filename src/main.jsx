b60ce52fc338e5a3af857c7b3d53239453272ddf
asejik <asejik@gmail.com>
2025-12-12 21:24:18 +0100

    fix: add mandatory PWA icons and explicit service worker registration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// --- ADD THESE LINES ---
+import { registerSW } from 'virtual:pwa-register'
+
+// Auto-update the app when a new version is available
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
+// -----------------------
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
